d0c32a0a7a66bb4e922134adcf24b4b2f4cd4db1
Crousthybalt <thibault.marette@gmail.com>
2018-11-02 02:33:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-/**
- * An example of how you can send embeds
- */
 
-// Extract the required classes from the discord.js module
 const { Client, RichEmbed, Attachment } = require('discord.js');
 
-// Create an instance of a Discord client
+
 const client = new Client();
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
   console.log('I am ready!');
 });
 var compteur_parole = 0
+
 client.on('message', message => {
 	var msg = message.content.toLowerCase()
 	var charlotte = "Aya_Kasai#3571"
