27b651fa34b8d7aa1ef21e1ddeb72a06b47802be
Crousthybalt <thibault.marette@gmail.com>
2018-11-04 17:22:26 +0100

    cleanup no spaghetti

---
+++ commands/reactions.js
@@ -0,0 +1,28 @@
+module.exports = {
+    name: 'reactions',
+    description: 'When i react without prefixes',
+    execute(message, args) {
+      if (msg.includes('olala')) {
+
+        message.channel.send('quelle vie');
+      }
+
+      if (msg.includes('nul')) {
+
+        message.channel.send(':(');
+     if (msg.includes('zoe')) {
+
+      message.channel.send("**NON**");
+      }
+      if (msg.includes('big hands')) {
+        message.channel.send('miam')
+      }
+      if (msg.includes('good bot')) {
+        message.react('😄');
+        message.channel.send('hihi : ]');
+        //message.channel.send('http://icons.iconarchive.com/icons/svengraph/daft-punk/512/Daft-Punk-Guyman-Smile-icon.png');
+      }
+      if (msg.includes('bonne nuit')) {
+        message.reply('fait de beaux rêves !! :sleeping:');
+      }
+  };
---
+++ main.js
@@ -0,0 +1,63 @@
+
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, personnes } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix)){client.commands.get('reactions').execute(message, args);}
+  if (message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		client.commands.get('ping').execute(message, args);
+	}
+
+		else if (args[0] === 'foo') {
+			return message.channel.send('bar');
+		}
+
+		message.channel.send(`First argument: ${args[0]}`);
+	}
+	else if (command === 'kick') {
+		if (!message.mentions.users.size) {
+			return message.reply('you need to tag a user in order to kick them!');
+		}
+
+		const taggedUser = message.mentions.users.first();
+
+		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+	}
+	else if (command === 'avatar') {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: ${message.author.displayAvatarURL}`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: ${user.displayAvatarURL}`;
+		});
+
+		message.channel.send(avatarList);
+	}
+});
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
