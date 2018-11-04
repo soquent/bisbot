5d242c311cd4733c9571f0859bae3a004a5800bd
Crousthybalt <thibault.marette@gmail.com>
2018-11-04 18:04:23 +0100

    with greetings

--- main.js
+++ main.js
@@ -13,7 +13,13 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('I\'m ready !');
+  const chans = client.channels.array()
+	const nbChans = chans.length
+	for (var i=0;	i < nbChans; i++){
+		var c = chans[i]
+		if (c.id === '507693816531386379') {c.send("bip boup. Mise à jour réussie. Je suis opérationnel ! :trumpet:")}
+  }
 });
 
 client.on('message', message => {
