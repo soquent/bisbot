27c6447d702ed2a2b6d04f50bc62bda150437e72
Crousthybalt <thibault.marette@gmail.com>
2018-11-05 21:14:07 +0100

    wiki unchained

--- commands/wiki.js
+++ commands/wiki.js
@@ -6,26 +6,28 @@ module.exports = {
 
   		  if (mots[0].includes( 'talent' )|| mots[0].includes('skill')){message.channel.send('http://fr.mogapedia.wikia.com/wiki/MHGU_-_Liste_des_talents')}
 
-  		  else if (mots[0] === 'list'){message.channel.send(liste_monstres);}
-  		  else{
+  		  if (mots[0] === 'list'){message.channel.send(liste_monstres);}
 
-  		  var monstre = mots[0].capitalize()
-  		  for (var i = 1; i < mots.length; i++) {
+        else{
 
-  			monstre = monstre.concat("_");
-  			if (mots[i].includes("-")) {
-  				const [a,b] = mots[i].split("-");
-  				monstre = monstre.concat(a.capitalize())
-  				monstre = monstre.concat("-")
-  				monstre = monstre.concat(b.capitalize())
-  			}
-  			else{
-  		  monstre = monstre.concat(mots[i].capitalize())
-  			}
+
+  		      var monstre = mots[0].capitalize()
+  		        for (var i = 1; i < mots.length; i++) {
+
+  			           monstre = monstre.concat("_");
+  			              if (mots[i].includes("-")) {
+  				                    const [a,b] = mots[i].split("-");
+  				                    monstre = monstre.concat(a.capitalize());
+  				                    monstre = monstre.concat("-");
+  				                    monstre = monstre.concat(b.capitalize());
+  			                       }
+  			           else{
+  		                  monstre = monstre.concat(mots[i].capitalize())
+  			                 }
   		  }
 
-  		  const wiki = "http://fr.mogapedia.wikia.com/wiki/".concat(monstre)
-  	  message.channel.send(wiki);
-}
-}
+  	     const wiki = "http://fr.mogapedia.wikia.com/wiki/".concat(monstre)
+  	      message.channel.send(wiki);
+        }
+      }
 }
