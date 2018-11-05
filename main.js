9dcb3192093b5dbf5f60e98eaf2b3f40eba8c396
Crousthybalt <thibault.marette@gmail.com>
2018-11-05 20:51:28 +0100

    2.0

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-//const prefix = require('./config.json');
-const prefix = "!";
+const prefix = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
