28c16931cde92be0d4db124dac853392e1859170
Crousthybalt <thibault.marette@gmail.com>
2018-11-05 20:23:11 +0100

    reprise des choses en main

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, personnes } = require('./config.json');
+const prefix = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -12,39 +12,68 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
-client.once('ready', () => {
-	console.log('I\'m ready !');
-	var chans = client.channels.array()
-	var nbChans = chans.length
-	for (var i=0;	i < nbChans; i++){
-		var c = chans[i]
-		if (c.id === '507693816531386379') {c.send("bip boup. Mise à jour réussie. Je suis opérationnel ! :trumpet:")
-	}
-}
+const cooldowns = new Discord.Collection();
 
+client.once('ready', () => {
+	console.log('Ready!');
 });
 
 client.on('message', message => {
-	if (message.author.bot) {return;}
-	if (!message.content.startsWith(prefix)) {client.commands.get('reactions').execute(message);}
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
 
-	if (!client.commands.has(command)) return;
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type !== 'text') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (!timestamps.has(message.author.id)) {
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	}
+	else {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+
+		timestamps.set(message.author.id, now);
+		setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+	}
 
 	try {
-		client.commands.get(command).execute(message, args);
+		command.execute(message, args);
 	}
 	catch (error) {
 		console.error(error);
-		message.reply('oopsie la commande ne s\'est pas déroulée comme prévue');
+		message.reply('there was an error trying to execute that command!');
 	}
-
 });
-
-
-
-
 
 client.login(process.env.BOT_TOKEN);
