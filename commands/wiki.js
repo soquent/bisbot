f8c0d75bf6bcbd1ab60215cdca7b7bdebd2d0ba0
Crousthybalt <thibault.marette@gmail.com>
2018-11-08 01:00:40 +0100

    )

--- commands/wiki.js
+++ commands/wiki.js
@@ -39,6 +39,6 @@ module.exports = {
           fetch('https://api.github.com/users/github')
     .then(res => res.json())
     .then(json => console.log(json));
-          })
+          }
         }
       }
