d688a0a4e03e908a22687064b3e4d7f6aae71fe9
Crousthybalt <thibault.marette@gmail.com>
2018-11-09 21:47:04 +0100

    V1.1

--- commands/loot.js
+++ commands/loot.js
@@ -47,7 +47,7 @@ module.exports = {
             const deb = doc.indexOf('<h3 class="pi-data-label pi-secondary-font">Weakest to:</h3>');
             const fin =  doc.indexOf('<h3 class="pi-data-label pi-secondary-font">Habitats:</h3>');
 
-            var sortie = ""
+            var sortie = "" //contains all elements the monster is weak to
             const doc2 = doc.substring(deb,fin);
             for (var i=0; i < elements.length; i++){
 
@@ -55,17 +55,18 @@ module.exports = {
                 sortie = sortie.concat(elements[i]+"\n")
                 }
               }
+              
               if (sortie === "") {return "This meownster doesn't even exists !";}
               if (Math.random() >.999) {sortie = sortie.concat("Also, this monster seems weak to Death :3")}
 
+
               const deb2 = doc.indexOf('<td colspan="2" style="background-color:#3A5766; color:#ffffff; font-weight:bold; font-size:9pt; text-align:center;"><b>Monster Hunter Generations</b>');
               const fin2 =  doc.indexOf('<b>Threat Level');
-
-              var doc_thumb = doc.substring(deb2,fin2);
+              var doc_thumb = doc.substring(deb2,fin2); //contains the thumbnail
               const deb3 = doc_thumb.indexOf('data-src=')+10;
               const fin3 = doc_thumb.indexOf('  	 width=')-1;
               doc_thumb = doc_thumb.substring(deb3,fin3);
-              message.channel.send(doc_thumb)
+
                 const embed = new Discord.RichEmbed()
                 .setTitle("Monster : ".concat(prettyname))
                 .setDescription(wiki)
