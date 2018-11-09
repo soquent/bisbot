447d80cce2b929f13af16ff541f30077039a9b4b
Crousthybalt <thibault.marette@gmail.com>
2018-11-09 21:05:43 +0100

    lothumb

--- commands/loot.js
+++ commands/loot.js
@@ -43,19 +43,34 @@ module.exports = {
 
 
             const elements = ['Fire','Ice','Dragon','Thunder','Water']
-            const deb = doc.indexOf('<td colspan="2" style="background-color:#3A5766; color:#ffffff; font-weight:bold; font-size:9pt; text-align:center;"><b>Monster Hunter Generations</b>');
-            const fin =  doc.indexOf('<b>Threat Level');
 
-            var doc2 = doc.substring(deb,fin);
-            const deb2 = doc2.indexOf('data-src=')+10;
-            const fin2 = doc2.indexOf('  	 width=')-1;
-            doc2 = doc2.substring(deb2,fin2);
+            const deb = doc.indexOf('<h3 class="pi-data-label pi-secondary-font">Weakest to:</h3>');
+            const fin =  doc.indexOf('<h3 class="pi-data-label pi-secondary-font">Habitats:</h3>');
+
+            var sortie = ""
+            const doc2 = doc.substring(deb,fin);
+            for (var i=0; i < elements.length; i++){
+
+              if (doc2.includes(elements[i])){
+                sortie = sortie.concat(elements[i]+"\n")
+                }
+              }
+              if (sortie === "") {return "This meownster doesn't even exists !";}
+              if (Math.random() >.999) {sortie = sortie.concat("Also, this monster seems weak to Death :3")}
+
+            const deb2 = doc.indexOf('<td colspan="2" style="background-color:#3A5766; color:#ffffff; font-weight:bold; font-size:9pt; text-align:center;"><b>Monster Hunter Generations</b>');
+            const fin2 =  doc.indexOf('<b>Threat Level');
+
+            var doc_thumb = doc.substring(deb2,fin2);
+            const deb3 = doc_thumb.indexOf('data-src=')+10;
+            const fin3 = doc_thumb.indexOf('  	 width=')-1;
+            doc_thumb = doc_thumb.substring(deb2,fin2);
 
               if (doc2 === "") {return "This meownster doesn't even exists !";}
               const embed = new Discord.RichEmbed()
               .setTitle("Monster : ".concat(prettyname))
               .setDescription(wiki)
-              .setThumbnail(doc2)
+              .setThumbnail(doc_thumb)
               .addField("Weaknesse(s) : ", doc2, true)
 
               .setColor("RANDOM")
