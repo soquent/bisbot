3e3234f9957410238e7ad747bc51126a9a8d5196
Crousthybalt <thibault.marette@gmail.com>
2018-11-09 23:38:37 +0100

    pali

--- commands/pali.js
+++ commands/pali.js
@@ -60,7 +60,15 @@ module.exports = {
               if (Math.random() >.999) {sortie = sortie.concat("Also, this monster seems weak to Death :3")}
 
 
-              const deb2 = doc.indexOf('<td colspan="2" style="background-color:#3A5766; color:#ffffff; font-weight:bold; font-size:9pt; text-align:center;"><b>Monster Hunter Generations</b>');
+
+              const alpha= doc.indexOf('<td colspan="2" style="background-color:#3A5766; color:#ffffff; font-weight:bold; font-size:9pt; text-align:center;"><b>Monster Hunter Generations</b>');
+              const beta = doc.indexOf('<td colspan="2" style="background-color:#3A5766; color:#ffffff; font-weight:bold; font-size:9pt; text-align:center;"><b>Monster Hunter 3</b>');
+              if doc.includes(alpha){
+                const deb2 = alpha;
+              }
+              else {
+                const deb2 = beta;
+              }
               const fin2 =  doc.indexOf('<b>Threat Level');
               var doc_thumb = doc.substring(deb2,fin2); //contains the thumbnail
               const deb3 = doc_thumb.indexOf('data-src=')+10;
