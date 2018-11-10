5fc1837a749b4bd48c6a7f185ac1f5335ed337c1
Crousthybalt <thibault.marette@gmail.com>
2018-11-10 14:09:25 +0100

    client

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ client.once('ready', () => {
 	const edbisbot = client.channels.find(channel => channel.name === "élevage-du-bisbot");
 	edbisbot.send("bip boup. Mise à jour réussie. Je suis opérationnel ! :trumpet:");
 	console.log(client.guilds.size.toString())
-	const bis = channel.users.find(user => user.id === "505055128705957889");
+	const bis = client.users.find(user => user.id === "505055128705957889");
 	bis.setActivity(`Type weak <monster> to get started ! \n${client.guilds.size.toString()} hunting `, { type: 'WATCHING' })
   .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
   .catch(console.error);
